731445fbf0b3ff5c2c8e8e32f261460d62c936cf
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-05-18 20:24:37 +0500

    done

--- Tabular Output/Program.cs
+++ Tabular Output/Program.cs
@@ -4,6 +4,8 @@
     {
         Console.WriteLine("N\t10*N\t100*N\t1000*N");
 
+        Console.WriteLine();
+
         for (int i = 1; i <= 5; i++)
         {
             Console.WriteLine(i + "\t" + i * 10 + "\t" + i * 100 + "\t" + i * 1000);

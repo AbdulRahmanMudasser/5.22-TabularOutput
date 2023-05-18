2a0b5c8a21a03ed4a28890c446e3786b8b26b853
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-05-18 20:23:12 +0500

    Add project files.

---
+++ Tabular Output/Program.cs
@@ -0,0 +1,12 @@
+﻿class TabularOutput
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("N\t10*N\t100*N\t1000*N");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i + "\t" + i * 10 + "\t" + i * 100 + "\t" + i * 1000);
+        }
+    }
+}
